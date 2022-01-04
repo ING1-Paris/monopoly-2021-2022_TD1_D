19e7606fc7ba977ace30a5d85798bcb9b0e9431c
jeremboyy <95629157+jeremboyy@users.noreply.github.com>
2022-01-04 12:25:10 +0100

    change

---
+++ structuresMonop/gotoligcol.c
@@ -0,0 +1,10 @@
+#include "headStruct.h"
+void gotoligcol( int lig, int col )
+{
+    // ressources
+    COORD mycoord;
+
+    mycoord.X = col;
+    mycoord.Y = lig;
+    SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), mycoord );
+}
